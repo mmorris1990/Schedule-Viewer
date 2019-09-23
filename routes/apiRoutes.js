d81a64ec116c39536e65c1c18d562ee06c77a1ac
Michael Morris <mmorris1990@gmail.com>
2019-09-23 15:41:08 -0500

    built all (hopefully) api routes. Need to add date functionality and possibly user routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,122 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+  // GET Today's scheduled jobs
+  app.get("/api/schedule/:today", function (req, res) {
+    db.sequelize.query("SELECT salesOrder, company, dateNotes, dateDue, shipping FROM schedules WHERE dateDue = '" + req.params.today + "'"
+      // DUE DATE = TODAY
+      ,
+      { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function (dbSchedule) {
+        res.json(dbSchedule);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // GET Tomorrows's scheduled jobs
+  app.get("/api/schedule/:tomorrow", function (req, res) {
+    db.sequelize.query("SELECT salesOrder, company, dateNotes, dateDue, shipping FROM schedules WHERE dateDue = '" + req.params.tomorrow + "'"
+      // DUE DATE = TOMORROW
+      ,
+      { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function (dbSchedule) {
+        res.json(dbSchedule);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // GET the next Week's scheduled jobs
+  app.get("/api/schedule/:week", function (req, res) {
+    db.sequelize.query("SELECT salesOrder, company, dateNotes, dateDue, shipping FROM schedules WHERE dateDue = '" + req.params.week + "'"
+      // DUE DATE = TODAY + 7 DAYS
+      ,
+      { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function (dbSchedule) {
+        res.json(dbSchedule);
+      });
   });
+
+  // GET all tasks by user
+  app.get("/api/task/task", function (req, res) {
+    db.sequelize.query("SELECT name, dueDate, description FROM tasks WHERE UserId = '" + req.user.id + " AND type = task", { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function (dbSchedule) {
+        res.json(dbSchedule);
+      });
+  });
+
+  // GET all projects by user
+  app.get("/api/task/project", function (req, res) {
+    db.sequelize.query("SELECT name, dueDate, description FROM tasks WHERE UserId = '" + req.user.id + " AND type = project", { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function (dbSchedule) {
+        res.json(dbSchedule);
+      });
+  });
+
+  // UPDATE task by task id
+  app.put("/api/task/task", function (req, res) {
+    db.Task.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      //------------ NEED TO ADD VALIDATION WHERE USER CAN ONLY UPDATE THEIR TASKS --------------
+      .then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // UPDATE project by task id
+  app.put("/api/task/project", function (req, res) {
+    db.Task.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      //------------ NEED TO ADD VALIDATION WHERE USER CAN ONLY UPDATE THEIR PROJECTS --------------
+      .then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // CREATE a new task
+  app.post("/api/task/task", function (req, res) {
+    console.log(req.body);
+    db.Task.create({
+      name: req.body.name,
+      dueDate: req.body.dueDate,
+      description: req.body.description,
+      type: "task",
+      UserId: req.user.id
+    })
+      .then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // CREATE a new project
+  app.post("/api/task/project", function (req, res) {
+    console.log(req.body);
+    db.Task.create({
+      name: req.body.name,
+      dueDate: req.body.dueDate,
+      description: req.body.description,
+      type: "project",
+      UserId: req.user.id
+    })
+      .then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // DELETE a task by id
+  app.delete("/api/task/:id", function (req, res) {
+    db.Task.destroy({ where: { id: req.params.id } })
+      .then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+  // GET User information
+
+  // CREATE a new user
 };
