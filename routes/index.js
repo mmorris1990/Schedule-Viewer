642fe80109525608472f8a805e111b4321412476
Michael Morris <mmorris1990@gmail.com>
2019-09-25 15:09:44 -0500

    reverted to non fucked up routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@ const axios = require("axios");
 const router = require("express").Router();
 const apiRoutes = require("./api/routes.js");
 
-
-// API Routes
-router.use("/api", apiRoutes);
-
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../client/public/index.html"));
